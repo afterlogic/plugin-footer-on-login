e6a9bba02b0e2cf54036f6c577fb62bcab91ab9c
afterlogic-support <support@afterlogic.com>
2016-10-20 19:07:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * Copyright (C) 2002-2015 AfterLogic Corp. (www.afterlogic.com)
- * Distributed under the terms of the license described in LICENSE
- *
- */
-
 class_exists('CApi') or die();
 
 class CFooterOnLoginPlugin extends AApiPlugin
